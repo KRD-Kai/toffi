83619b56f92bcf3abc3f568850e8f853a5069b7a
Kai Morciniec <kaimorciniec@gmail.com>
2022-07-18 23:50:10 +0200

    add rainbowkit dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
-import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import {
+    RainbowKitProvider,
+    getDefaultWallets,
+    darkTheme,
+} from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
@@ -41,7 +45,7 @@ const wagmiClient = createClient({
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <WagmiConfig client={wagmiClient}>
-            <RainbowKitProvider chains={chains}>
+            <RainbowKitProvider chains={chains} theme={darkTheme()}>
                 <Component {...pageProps} />
             </RainbowKitProvider>
         </WagmiConfig>
