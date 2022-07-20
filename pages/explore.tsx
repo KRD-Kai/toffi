34950d4c3c2954e13b48c04fe728710658036ef2
Kai Morciniec <kaimorciniec@gmail.com>
2022-07-20 19:37:47 +0200

    add nft searchbar

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { Hero, Input, Button, InputGroup, Form } from "react-daisyui";
 
 const Explore: NextPage = () => {
     return (
@@ -7,7 +8,25 @@ const Explore: NextPage = () => {
             <Head>
                 <title>Explore - Toffi</title>
             </Head>
-            Explore
+            <Hero className="pt-10">
+                <Hero.Content className="text-center">
+                    <div className="max-w-lg">
+                        <h1 className="text-5xl font-bold pb-4">
+                            Find and bid on NFTs
+                        </h1>
+                        <Form>
+                            <InputGroup className="justify-center">
+                                <Input
+                                    type="text"
+                                    className="w-full"
+                                    placeholder="Search NFT"
+                                />
+                                <Button>Search</Button>
+                            </InputGroup>
+                        </Form>
+                    </div>
+                </Hero.Content>
+            </Hero>
         </>
     );
 };
