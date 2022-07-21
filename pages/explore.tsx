20cf19934609e88c18249f04ae7a3c9b98dd9018
Kai Morciniec <kaimorciniec@gmail.com>
2022-07-21 14:27:35 +0200

    add submitHandler to search form

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -1,32 +1,36 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Hero, Input, Button, InputGroup, Form } from "react-daisyui";
+import { FormEvent, useState } from "react";
+import { Input, Button, InputGroup } from "react-daisyui";
 
 const Explore: NextPage = () => {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    function handleSearchSubmit(e: FormEvent) {
+        e.preventDefault();
+        alert(searchQuery);
+    }
     return (
         <>
             <Head>
                 <title>Explore - Toffi</title>
             </Head>
-            <Hero className="pt-10">
-                <Hero.Content className="text-center">
-                    <div className="max-w-lg">
-                        <h1 className="text-5xl font-bold pb-4">
-                            Find and bid on NFTs
-                        </h1>
-                        <Form>
-                            <InputGroup className="justify-center">
-                                <Input
-                                    type="text"
-                                    className="w-full"
-                                    placeholder="Search NFT"
-                                />
-                                <Button>Search</Button>
-                            </InputGroup>
-                        </Form>
-                    </div>
-                </Hero.Content>
-            </Hero>
+            <div className="max-w-lg pt-5 text-center m-auto">
+                <h1 className="text-4xl font-bold pb-4">
+                    Find and bid on NFTs
+                </h1>
+                <form onSubmit={handleSearchSubmit}>
+                    <InputGroup className="justify-center">
+                        <Input
+                            type="text"
+                            className="w-full"
+                            placeholder="Search NFT"
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                        />
+                        <Button>Search</Button>
+                    </InputGroup>
+                </form>
+            </div>
         </>
     );
 };
