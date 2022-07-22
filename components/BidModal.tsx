230ac54fdfa8bfd72bbb09b7c98c24c50c872470
Kai Morciniec <kaimorciniec@gmail.com>
2022-07-22 18:20:49 +0200

    add NFT contract address and tokenID display to BidModal

--- components/BidModal.tsx
+++ components/BidModal.tsx
@@ -26,7 +26,41 @@ export default function BidModal({
                 Place bid on {NFTData.name}
             </Modal.Header>
 
-            <Modal.Body>Body</Modal.Body>
+            <div className="divider h-0"></div>
+
+            <Modal.Body>
+                <div>
+                    Address:{" "}
+                    <a
+                        className="link link-accent"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={`https://etherscan.io/address/${NFTData.contract_address}`}
+                    >
+                        {NFTData.contract_address.substring(0, 6)}...
+                        {NFTData.contract_address.slice(-3)}
+                    </a>
+                </div>
+                <div>
+                    Token ID:{" "}
+                    <a
+                        className="link link-accent"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={`https://etherscan.io/token/${NFTData.contract_address}?a=${NFTData.token_id}`}
+                    >
+                        {NFTData.token_id.length > 8 ? (
+                            <>
+                                {" "}
+                                {NFTData.token_id.substring(0, 3)}...
+                                {NFTData.token_id.slice(-3)}
+                            </>
+                        ) : (
+                            NFTData.token_id
+                        )}
+                    </a>
+                </div>
+            </Modal.Body>
 
             <Modal.Actions>
                 <Button>Place bid</Button>
