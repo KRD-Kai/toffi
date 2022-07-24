062003a83958a16d4031ec33e84093983f418647
Kai Morciniec <kaimorciniec@gmail.com>
2022-07-24 12:23:59 +0200

    add offers page

---
+++ src/pages/offers.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Offers: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Offers - Toffi</title>
+            </Head>
+            Offers
+        </>
+    );
+};
+
+export default Offers;
